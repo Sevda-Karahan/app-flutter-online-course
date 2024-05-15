dbe1cd591b10c9cdae0c6b63a68b69f7477f340e
Sevda KARAHAN <116480291+Sevda-Karahan@users.noreply.github.com>
2024-05-15 22:21:51 +0300

    Delete Online-Course-App-Flutter directory

--- Online-Course-App-Flutter/lib/core/utils/network_url.dart
+++
@@ -1,5 +0,0 @@
-class NetworkUrls {
-  static const String baseUrl = 'http://localhost:8080/course_project';
-  static const String apiKey = '';
-  static const String getCourses = '$baseUrl/course';
-}
--- Online-Course-App-Flutter/lib/core/utils/typedef.dart
+++
@@ -1,5 +0,0 @@
-import 'package:dartz/dartz.dart';
-import 'package:online_course/core/errors/failure.dart';
-
-typedef ResultFuture<T> = Future<Either<Failure, T>>;
-typedef DataMap = Map<String, dynamic>;
